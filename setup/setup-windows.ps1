112d1683d2edc5210910f13fb862c4a37a6e3242
Dave Rael <dave@raelyard.com>
2023-03-27 09:19:52 -0600

    Remove unnecessary setup of WSL instance
    because it's not needed and is too large a download and installation to ask students to do it

--- setup/setup-windows.ps1
+++ setup/setup-windows.ps1
@@ -1,9 +1,5 @@
 choco install -y Microsoft-Windows-Subsystem-Linux -source WindowsFeatures
 
-Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
-Add-AppxPackage .\Ubuntu.appx
-rm  .\Ubuntu.appx
-
 choco install -y docker-desktop
 choco install -y kubernetes-helm
 choco install -y kind
